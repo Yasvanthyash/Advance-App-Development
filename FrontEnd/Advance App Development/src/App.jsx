d4aff0fb4ea48f2eeaadf367e2be56647382166b
Yasvanthyash <yasvanthyash99@gmail.com>
2024-03-19 11:29:42 +0530

    Day 2

--- FrontEnd/Advance App Development/src/App.jsx
+++ FrontEnd/Advance App Development/src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React, { useEffect } from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import UserLogin from './Components/Login'
+import UserRegister from './Components/Register'
+import Homee from './Components/HomePage';
+import AdminLogin from './Components/AdminLogin';
+import AdminRegister from './Components/AdminRegister';
+import AdminDashboard from './Components/AdminDashboard';
+import CreateJob from './Components/CreateJob';
+import ApplicantDashboard from './Components/ApplicantDashboard';
+import JobPosting from './Components/JobPosting';
+import About from './Components/About';
+import PaymentPage from './Components/Payment';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    document.title = 'JOB SPARK'; 
+  }, []);
   return (
     <>
-      <div>
-        Hello World !
-      </div>
+    <div>
+     <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<Homee/>}></Route>
+     <Route path="/applicantlogin" element ={<UserLogin/>}></Route>
+     <Route path="/applicantsignup" element={<UserRegister/>}></Route>
+     <Route path="/adminlogin" element={<AdminLogin/>}></Route>
+     <Route path="/adminsignup" element={<AdminRegister/>}></Route>
+     <Route path="/admindashboard" element={<AdminDashboard/>}></Route>
+     <Route path="/applicantdashboard" element={<ApplicantDashboard/>}></Route>
+     <Route path="/createjob" element={<CreateJob/>}></Route>
+     <Route path="/jobposting" element={<JobPosting/>}></Route>
+     <Route path="/about" element={<About/>}></Route>
+     <Route path="/payment" element={<PaymentPage/>}></Route>
+     </Routes>
+     </BrowserRouter></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FrontEnd/Advance App Development/src/Components/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Login.css';
+import { Link, useNavigate } from 'react-router-dom';
+function UserLogin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const nav=useNavigate();
+
+  const authenticate = (e) => {
+    e.preventDefault();
+
+    const trimmedUsername = username.trim();
+    const trimmedPassword = password.trim();
+
+    if (trimmedUsername.length === 0) {
+      alert('Enter Username');
+    } else if (trimmedPassword.length === 0) {
+      alert('Enter Password');
+    } else {
+      alert("Logged in Successfully!!");
+      nav('/applicantdashboard')
+    }
+  };
+
+  return (
+    <div id="body">
+      <div className="login-form">
+        <div className="containerr">
+          <div className="header">
+            <h1>Job Application Portal</h1>
+            <p>Applicant Login</p>
+          </div>
+
+          <form>
+            <div className="input">
+              <input
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+            </div>
+            <div className="input">
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </div>
+            <input
+              onClick={authenticate}
+              className="login-button"
+              type="submit"
+              value="LOGIN"
+            />
+          </form>
+          <Link to="/applicantsignup">
+            <p>Don't have an account? Register</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserLogin;
